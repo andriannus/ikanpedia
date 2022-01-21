c90ba9f9bd09cb5fde7553da86028f43c71a858f
Andriannus Parasian <andriannus.parasian@qoala.id>
2022-01-21 16:16:29 +0700

    chore: install and setup `font-awesome`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from "next/app";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
+
+library.add(faChevronRight);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
