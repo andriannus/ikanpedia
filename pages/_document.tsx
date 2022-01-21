e302eb7bc8851b19d8f41e9c715e53e79af7f7c4
Andriannus Parasian <andriannus.parasian@qoala.id>
2022-01-21 20:54:34 +0700

    refactor: update document title and class

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,13 +17,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Data Harga Ikan Terlengkap - Ikanpedia</title>
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap"
             rel="stylesheet"
           />
         </Head>
 
-        <body>
+        <body className="Document">
           <Main />
           <NextScript />
         </body>
