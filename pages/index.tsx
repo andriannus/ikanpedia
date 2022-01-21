43a88cec9ddc15f8e6e15729428b0b42b22850e1
Andriannus Parasian <andriannus.parasian@qoala.id>
2022-01-21 20:55:19 +0700

    feat: create static home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,156 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { AppBar } from "@/components/app-bar";
+import { Box } from "@/components/box";
+import { Chip } from "@/components/chip";
+
+import appBarStyles from "@/components/app-bar/app-bar.module.scss";
+import homeStyles from "@/styles/home.module.scss";
+
+interface Filter {
+  city: string;
+  province: string;
+  size: string;
+}
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const [filter, setFilter] = useState<Partial<Filter>>({
+    city: "",
+    province: "",
+    size: "",
+  });
+
+  function selectCity(city: string): void {
+    const validCity = filter.city === city ? "" : city;
+    setFilter({ ...filter, city: validCity });
+  }
+
+  function selectProvince(province: string): void {
+    const validProvince = filter.province === province ? "" : province;
+    setFilter({ ...filter, province: validProvince });
+  }
+
+  function selectSize(size: string): void {
+    const validSize = filter.size === size ? "" : size;
+    setFilter({ ...filter, size: validSize });
+  }
+
+  function navigateToDetailPage(): void {
+    router.push("/detail");
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <AppBar>
+        <span className={appBarStyles["AppBar-brand"]}>Ikanpedia</span>
+      </AppBar>
+
+      <Box className="mb-md">
+        <p className="font-bold mb-bs text-base leading-5 text-gray-900">
+          Filter
+        </p>
+
+        <div className="mb-bs">
+          <p className="mb-sm text-sm leading-5">Ukuran</p>
+
+          {["25", "50", "200", "100"].map((size) => {
+            return (
+              <Chip
+                active={filter.size === size}
+                button
+                className="mr-xs"
+                onClick={() => selectSize(size)}
+              >
+                {size}
+              </Chip>
+            );
+          })}
+        </div>
+
+        <div className="mb-bs">
+          <p className="mb-sm text-sm leading-5">Provinsi</p>
+
+          {["Jawa Barat", "Sumatera Utara", "Lampung", "Bali"].map(
+            (province) => {
+              return (
+                <Chip
+                  active={filter.province === province}
+                  button
+                  className="mr-xs"
+                  onClick={() => selectProvince(province)}
+                >
+                  {province}
+                </Chip>
+              );
+            },
+          )}
+        </div>
+
+        <div>
+          <p className="mb-sm text-sm leading-5">Kota</p>
+
+          {["Bogor", "Jakarta Selatan", "Medan", "Denpasar"].map((city) => {
+            return (
+              <Chip
+                active={filter.city === city}
+                button
+                className="mr-xs"
+                onClick={() => selectCity(city)}
+              >
+                {city}
+              </Chip>
+            );
+          })}
+        </div>
+      </Box>
+
+      <Box>
+        <p className="font-bold mb-bs text-base leading-5 text-gray-900">
+          Data Komuditas Ikan
+        </p>
+
+        <ul className={homeStyles["List"]}>
+          <li
+            className={homeStyles["List-item"]}
+            onClick={navigateToDetailPage}
+          >
+            <div className={homeStyles["List-itemContent"]}>
+              <p className={homeStyles["List-itemTitle"]}>Ikan Bawal</p>
+
+              <strong className={homeStyles["List-itemSubtitle"]}>
+                Rp50.000
+              </strong>
+            </div>
+
+            <div className={homeStyles["List-itemAction"]}>
+              <FontAwesomeIcon className="text-ruby-500" icon="chevron-right" />
+            </div>
+          </li>
+
+          <li
+            className={homeStyles["List-item"]}
+            onClick={navigateToDetailPage}
+          >
+            <div className={homeStyles["List-itemContent"]}>
+              <p className={homeStyles["List-itemTitle"]}>Ikan Bandeng</p>
+
+              <strong className={homeStyles["List-itemSubtitle"]}>
+                Rp72.000
+              </strong>
+            </div>
+
+            <div className={homeStyles["List-itemAction"]}>
+              <FontAwesomeIcon className="text-ruby-500" icon="chevron-right" />
+            </div>
+          </li>
+        </ul>
+      </Box>
+    </>
   );
 };
 
