7548a023fa5f6d5f5a3c57d5947124dff39133d4
Andriannus Parasian <andriannus.parasian@qoala.id>
2022-01-22 22:35:52 +0700

    feat: create `api-invoker` service

---
+++ services/api-invoker/api-invoker.service.ts
@@ -0,0 +1,64 @@
+import SteinStore from "stein-js-client";
+
+interface ApiInvokerService {
+  fetchAreas(): Promise<Areas>;
+  fetchCommodities(): Promise<Commodity[]>;
+  fetchSizes(): Promise<string[]>;
+}
+
+export interface Area {
+  city: string;
+  province: string;
+}
+
+interface Areas {
+  areas: Area[];
+  cities: string[];
+  provinces: string[];
+}
+
+export interface Commodity {
+  area_kota: string;
+  area_provinsi: string;
+  komoditas: string;
+  price: string;
+  size: string;
+  tgl_parsed: string;
+  timestamp: string;
+  uuid: string;
+}
+
+export function useApiInvoker(): ApiInvokerService {
+  const STEIN_URL =
+    "https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4";
+
+  const apiInvoker = new SteinStore(STEIN_URL);
+
+  async function fetchCommodities(): Promise<Commodity[]> {
+    const commodities: Commodity[] = await apiInvoker.read("list");
+    const validCommodities = commodities.filter((commodity) => {
+      return !!commodity.uuid;
+    });
+
+    return validCommodities;
+  }
+
+  async function fetchAreas(): Promise<Areas> {
+    const areas = await apiInvoker.read("option_area");
+    const cities = areas.map((area: Area) => area.city);
+    const provinces = Array.from<string>(
+      new Set(areas.map((area: Area) => area.province)),
+    );
+
+    return { areas, cities, provinces };
+  }
+
+  async function fetchSizes(): Promise<string[]> {
+    const sizes = await apiInvoker.read("option_size");
+    const validSizes = sizes.map(({ size }: { size: string }) => size);
+
+    return validSizes;
+  }
+
+  return { fetchCommodities, fetchAreas, fetchSizes };
+}
