ab9abbb098a489a21216e8bdfb2347e1970338aa
Andriannus Parasian <andriannus.parasian@qoala.id>
2022-01-23 21:16:10 +0700

    feat: add new transform function

--- utils/date/date.constant.ts
+++ utils/date/date.constant.ts
@@ -1,1 +1,2 @@
 export const DATE_LONG_FORMAT = "dd MMMM yyyy";
+export const DATE_TIME_LONG_ISO_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
--- utils/date/date.util.ts
+++ utils/date/date.util.ts
@@ -1,10 +1,14 @@
 import { format } from "date-fns";
 import { id } from "date-fns/locale";
 
-import { DATE_LONG_FORMAT } from "./date.constant";
+import { DATE_LONG_FORMAT, DATE_TIME_LONG_ISO_FORMAT } from "./date.constant";
 
 export function transformToDateLongFormat(date: Date): string {
   return format(date, DATE_LONG_FORMAT, {
     locale: id,
   });
+}
+
+export function transformToDateTimeLongIsoFormat(date: Date): string {
+  return format(date, DATE_TIME_LONG_ISO_FORMAT);
 }
